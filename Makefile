55e5fd5a1918a6f673c0ca087927bf58652c63de
Amit Barzilai <amit.barzilai22@gmail.com>
2025-08-24 23:17:40 +0300

    Integrated analysis in sniffer

--- Makefile
+++ Makefile
@@ -1,12 +1,32 @@
+# Directories and Files
+BUILD_DIR = build
+SNIFFER_DIR = sniffer
+ANALYSIS_DIR = analysis
+SNIFFER_TARGET = $(BUILD_DIR)/sniffer_app
+
+# Compiler
 CC := gcc
-CFLAGS := -g
-BUILD_DIR := build
-SNIFFER_BIN := sniffer
+CFLAGS := -g -Wall -Wextra
+LDFLAGS = -lpcap
 
-build-sniffer:
-	mkdir -p $(BUILD_DIR)
-	$(CC) $(CFLAGS) sniffer/main.c -lpcap -o $(BUILD_DIR)/$(SNIFFER_BIN)
+CFLAGS += -I$(ANALYSIS_DIR)
 
-sniff: $(BUILD_DIR)/$(SNIFFER_BIN)
-	sudo $(BUILD_DIR)/$(SNIFFER_BIN)
+# Sniffer
+SRC := $(wildcard $(SNIFFER_DIR)/*.c $(ANALYSIS_DIR)/*.c)
+OBJ := $(patsubst %.c,$(BUILD_DIR)/%.o,$(SRC))
+
+sniffer: $(SNIFFER_TARGET)
+
+$(SNIFFER_TARGET): $(OBJ)
+	$(CC) $(CFLAGS) -o $@ $^ $(LDFLAGS)
+
+$(BUILD_DIR)/%.o: %.c
+	@mkdir -p $(dir $@)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+sniff: $(SNIFFER_TARGET)
+	sudo $(SNIFFER_TARGET)
+
+clean:
+	rm -rf $(SNIFFER_TARGET) $(BUILD_DIR)
 
--- sniffer/main.c
+++ sniffer/main.c
@@ -1,3 +1,4 @@
+#include "frame_analysis.h"
 #include <net/ethernet.h>
 #include <string.h>
 #include <pcap/pcap.h>
@@ -11,76 +12,81 @@
 #include <arpa/inet.h>
 #include <netinet/if_ether.h> /* includes net/ethernet.h */
 
-void parse_ip_packet(const u_char *packet)
-{
-    struct in_addr src, dst;
-    struct ip *ip_hdr = (struct ip *)(packet + sizeof(struct ether_header));
-
-    src.s_addr = ip_hdr->ip_src.s_addr;
-    dst.s_addr = ip_hdr->ip_dst.s_addr;
-
-    printf("\t\tFrom: %s\n", inet_ntoa(src));
-    printf("\t\tTo: %s\n", inet_ntoa(dst));
-    printf("\tVersion: %d\n", (unsigned int)ip_hdr->ip_v);
-    printf("\tHeader Length: %d bytes\n",((unsigned int)(ip_hdr->ip_hl))*4);
-    printf("\tType of Service: %d\n", (unsigned int)ip_hdr->ip_tos);
-    printf("\tTotal Length: %d  bytes\n", ntohs(ip_hdr->ip_len));
-    printf("\tIdentification: %d\n", ntohs(ip_hdr->ip_id));
-    printf("\tTime to live: %d\n", (unsigned int)ip_hdr->ip_ttl);
-    printf("\tProtocol: %d\n", (unsigned int)ip_hdr->ip_p);
-    printf("\tChecksum: %d\n", ntohs(ip_hdr->ip_sum));
-}
-
-void parse_ethernet_packet(const u_char *packet, struct pcap_pkthdr *hdr)
-{
-    int i;
-    struct ether_header *eptr;  /* net/ethernet.h */
-    u_char *ptr; /* printing out hardware header info */
-
-    printf("Grabbed packet of length %d\n",hdr->len);
-    printf("Recieved at ..... %s\n",ctime((const time_t*)&hdr->ts.tv_sec)); 
-    printf("Ethernet address length is %d\n",ETHER_HDR_LEN);
-
-    /* lets start with the ether header... */
-    eptr = (struct ether_header *) packet;
-
-    /* check to see if we have an ip packet */
-    if (ntohs (eptr->ether_type) == ETHERTYPE_IP)
-    {
-        printf("Ethernet type hex:%x dec:%d is an IP packet\n",
-                ntohs(eptr->ether_type),
-                ntohs(eptr->ether_type));
-        parse_ip_packet(packet);
-    }else  if (ntohs (eptr->ether_type) == ETHERTYPE_ARP)
-    {
-        printf("Ethernet type hex:%x dec:%d is an ARP packet\n",
-                ntohs(eptr->ether_type),
-                ntohs(eptr->ether_type));
-    }else {
-        printf("Ethernet type %x not IP", ntohs(eptr->ether_type));
-        exit(1);
-    }
-
-    ptr = eptr->ether_dhost;
-    i = ETHER_ADDR_LEN;
-    printf(" Destination Address:  ");
-    do{
-        printf("%s%x",(i == ETHER_ADDR_LEN) ? " " : ":",*ptr++);
-    }while(--i>0);
-    printf("\n");
-
-    ptr = eptr->ether_shost;
-    i = ETHER_ADDR_LEN;
-    printf(" Source Address:  ");
-    do{
-        printf("%s%x",(i == ETHER_ADDR_LEN) ? " " : ":",*ptr++);
-    }while(--i>0);
-    printf("\n");
-}
+// char *get_packet_data(const u_char *packet)
+// {
+// }
+//
+// void parse_ip_packet(const u_char *packet)
+// {
+//     struct in_addr src, dst;
+//     struct ip *ip_hdr = (struct ip *)(packet + sizeof(struct ether_header));
+//
+//     src.s_addr = ip_hdr->ip_src.s_addr;
+//     dst.s_addr = ip_hdr->ip_dst.s_addr;
+//
+//     printf("\t\tFrom: %s\n", inet_ntoa(src));
+//     printf("\t\tTo: %s\n", inet_ntoa(dst));
+//     printf("\tVersion: %d\n", (unsigned int)ip_hdr->ip_v);
+//     printf("\tHeader Length: %d bytes\n",((unsigned int)(ip_hdr->ip_hl))*4);
+//     printf("\tType of Service: %d\n", (unsigned int)ip_hdr->ip_tos);
+//     printf("\tTotal Length: %d  bytes\n", ntohs(ip_hdr->ip_len));
+//     printf("\tIdentification: %d\n", ntohs(ip_hdr->ip_id));
+//     printf("\tTime to live: %d\n", (unsigned int)ip_hdr->ip_ttl);
+//     printf("\tProtocol: %d\n", (unsigned int)ip_hdr->ip_p);
+//     printf("\tChecksum: %d\n", ntohs(ip_hdr->ip_sum));
+// }
+//
+// void parse_ethernet_packet(const u_char *packet, struct pcap_pkthdr *hdr)
+// {
+//     int i;
+//     struct ether_header *eptr;  /* net/ethernet.h */
+//     u_char *ptr; /* printing out hardware header info */
+//
+//     printf("Grabbed packet of length %d\n",hdr->len);
+//     printf("Recieved at ..... %s\n",ctime((const time_t*)&hdr->ts.tv_sec)); 
+//     printf("Ethernet address length is %d\n",ETHER_HDR_LEN);
+//
+//     /* lets start with the ether header... */
+//     eptr = (struct ether_header *) packet;
+//
+//     /* check to see if we have an ip packet */
+//     if (ntohs (eptr->ether_type) == ETHERTYPE_IP)
+//     {
+//         printf("Ethernet type hex:%x dec:%d is an IP packet\n",
+//                 ntohs(eptr->ether_type),
+//                 ntohs(eptr->ether_type));
+//         parse_ip_packet(packet);
+//     }else  if (ntohs (eptr->ether_type) == ETHERTYPE_ARP)
+//     {
+//         printf("Ethernet type hex:%x dec:%d is an ARP packet\n",
+//                 ntohs(eptr->ether_type),
+//                 ntohs(eptr->ether_type));
+//     }else {
+//         printf("Ethernet type %x not IP", ntohs(eptr->ether_type));
+//         exit(1);
+//     }
+//
+//     ptr = eptr->ether_dhost;
+//     i = ETHER_ADDR_LEN;
+//     printf(" Destination Address:  ");
+//     do{
+//         printf("%s%x",(i == ETHER_ADDR_LEN) ? " " : ":",*ptr++);
+//     }while(--i>0);
+//     printf("\n");
+//
+//     ptr = eptr->ether_shost;
+//     i = ETHER_ADDR_LEN;
+//     printf(" Source Address:  ");
+//     do{
+//         printf("%s%x",(i == ETHER_ADDR_LEN) ? " " : ":",*ptr++);
+//     }while(--i>0);
+//     printf("\n");
+// }
 
 int main(int argc, char **argv)
 {
     int i;
+    char *payload;
     char *dev; 
     char errbuf[PCAP_ERRBUF_SIZE];
     pcap_t* descr;
@@ -150,7 +156,11 @@ int main(int argc, char **argv)
         exit(1);
     }
 
-    parse_ethernet_packet(packet, &hdr);
+    payload = (char *)find_tcp_payload(packet, hdr.len);
+    if (payload)
+        printf("Payload: %s\n", payload);
+    else 
+        printf("No payload found!\n");
 
     pcap_close(descr);
     pcap_freealldevs(all_devices);
